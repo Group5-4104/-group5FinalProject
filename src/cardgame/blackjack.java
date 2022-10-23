d69624d41afaf57102254ab18508dffde1d92802
DWashin <106340886+DWashin@users.noreply.github.com>
2022-10-23 09:14:10 -0700

    Add files via upload

---
+++ src/cardgame/blackjack.java
@@ -0,0 +1,19 @@
+package blackjackgame;
+
+public class blackjack {
+
+	private int value;
+
+	public int getValue() {
+		return this.value;
+	}
+
+	/**
+	 * 
+	 * @param value
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
