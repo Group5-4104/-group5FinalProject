0f230d65a9cf98789b65f3fa1f31dca5062ecb44
wilstari <34189191+wilstari@users.noreply.github.com>
2022-12-08 19:22:48 -0500

    hand class ad blackjack class //Tarick Wilson

--- src/blackjackgame/BlackJackGame.java
+++ src/blackjackgame/BlackJackGame.java
@@ -5,6 +5,7 @@
 package blackjackgame;
 
 import blackjackgame.Deck.*;
+import java.util.Scanner;
 /**
  *
  * @author Will C-S
@@ -12,7 +13,12 @@ import blackjackgame.Deck.*;
 public class BlackJackGame {
 
  static Card[] deck;
+ static int i=0;
+ static Scanner input = new Scanner (System.in);
     public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        
        startGame();
        game();
         
@@ -26,7 +32,7 @@ public class BlackJackGame {
 		System.out.println("");
   System.out.println(" RULES: ");
 		System.out.println("* You will be dealt 2 cards. The dealer is dealt 2 cards with one face-up and one face-down.");
-		System.out.println("* Face cards are worth 10, an ace is worth 1 or 11 and cards are their value.");
+		System.out.println("* Face cards are worth 10, an ace is worth 11 and cards are their value.");
 		System.out.println("* Cards are added up for their total.");
 		System.out.println("* To obtain another card from the deck, players must \"Hit.\" To maintain their current card total, players must \"Stay.\"");
 		System.out.println("* Dealer \"Hits\" till they reach 17 or more.");
@@ -37,23 +43,69 @@ public class BlackJackGame {
 		System.out.println("");
                 
                   deck = Deck.shuffleDeck(Deck.generateDeck());
-        //System.out.println("Here are the cards in the hand");
-       //  for(Card card:deck){
-         //    System.out.printf("%s of %s\n",card.getValue(), card.getSuit());
-        // }
+                  shoe();
+       /** System.out.println("Here are the cards in the hand");
+        for(Card card:deck){
+            System.out.printf("%s of %s\n",card.getValue(), card.getSuit());
+        }**/
     }
     
     
     public static void game(){
-       for (int i=0;i<3;i++){ 
-       Hand.setpHand(deck);
-       }
-      Card[] c=Hand.getpHand();
+      int hos=5;
+       
+       
       
-       for (int i=0;i<3;i++){ 
-     //  System.out.println(c[i].getValue());
-       }
+       
+       System.out.println("To Stand Enter 0 to Hit enter 1");
+      
+      hos = input.nextInt();
+       
+      if(hos==0){
+         
+        hos=0; 
+        Hand.calculateHandp(hos);
+      }
+      if(hos==1){
+          hos=1;
+          Hand.setpHand(deck,BlackJackGame.i);
+          System.out.printf("%s of %s\n",Hand.getpHand()[0].getValue(), Hand.getpHand()[0].getSuit());
+          Hand.setdHand(deck,BlackJackGame.i);
+       BlackJackGame.i++; 
+       Hand.calculateHandp(hos);
+       game();
+       
+       
+      }
+      
+      
+     
+      
+      
+       System.out.printf("%s of %s\n",Hand.getpHand()[0].getValue(), Hand.getpHand()[0].getSuit());
+       //System.out.printf("%s of %s\n",d[0].getValue(), d[0].getSuit());
+        //System.out.printf("%s of %s\n",c[2].getValue(), c[2].getSuit());
+       //System.out.printf("%s of %s\n",d[1].getValue(), d[1].getSuit());
+       
       
     }
+    
+    public static void shoe(){
+         int hos=1;
+        for (int i=0; i<2;i++){
+            Hand.setpHand(deck,BlackJackGame.i);
+       BlackJackGame.i++;
+          //Hand.setdHand(deck,BlackJackGame.i);
+       //BlackJackGame.i++;
+       
+        } 
+        
+        System.out.printf("%s of %s\n",Hand.getpHand()[0].getValue(), Hand.getpHand()[0].getSuit());
+        System.out.printf("%s of %s\n",Hand.getpHand()[1].getValue(), Hand.getpHand()[1].getSuit());
+        
+        
+    }
+    
+   
     
 }
--- src/blackjackgame/Hand.java
+++ src/blackjackgame/Hand.java
@@ -8,21 +8,241 @@ package blackjackgame;
  */
 class Hand {
 private static Card[] pHand = new Card[12];
-private static int i=0;
+private static Card[] dHand = new Card[12];
+
+private static int cPhand=0;
+private static int cDhand=0;
     public static Card[] getpHand() {
         return pHand;
     }
 
-    public static void setpHand(Card[] pHand) {
-        Hand.pHand[i] = pHand[i];
-        i++;
-        
-     /**  for(int i=0;i< Hand.i ;i++){
-             System.out.printf("%s of %s\n",pHand[i].getValue(), pHand[i].getSuit());
-         }**/
+    public static void setpHand(Card[] pHand,int no) {
+        Hand.pHand[cPhand] = pHand[no];
+       cPhand++;
+    
+       
+          // System.out.printf("%s of %s\n",Hand.pHand[no].getValue(), Hand.pHand[no].getSuit());
+       
+               
+              
+         
     }
 
 
+    public static Card[] getdHand() {
+        return dHand;
+    }
 
+    public static void setdHand(Card[] dHand,int no) {
+        Hand.dHand[cDhand] = dHand[no];
+        cDhand++;
+        //System.out.printf("%s of %s\n",Hand.dHand[no].getValue(), Hand.dHand[no].getSuit());
+ 
+    }
+    
+    
+    public static int calculateHandp(int hos){
+        int j=0;
+        int handValue=0;
+        int aceCount=0;
+        while (Hand.pHand[j] !=null){
+            String o;
+            o = Hand.pHand[j].getValue().toString();
+            
+           //System.out.println(o);
+           
+            switch(o){
+                case "ACE":
+                         handValue+=11;
+                         System.out.println("11");
+                       
+                    break;
+                    case "TWO":
+                    System.out.println("2");
+                    handValue+=2;
+                    break;
+                    case "THREE":
+                    System.out.println("3");
+                    handValue+=3;
+                    break;
+                    case "FOUR":
+                    System.out.println("4");
+                    handValue+=4;
+                    break;
+                    case "FIVE":
+                    System.out.println("5");
+                    handValue+=5;
+                    break;
+                    case "SIX":
+                    System.out.println("6");
+                    handValue+=6;
+                    break;
+                    case "SEVEN":
+                    System.out.println("7");
+                    handValue+=7;
+                    break;
+                    case "EIGHT":
+                    System.out.println("8");
+                    handValue+=8;
+                    break;
+                    case "NINE":
+                    System.out.println("9");
+                    handValue+=9;
+                    break;
+                    case "TEN":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "JACK":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "QUEEN":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "KING":
+                    System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    
+            }
+            
+            
+           
+            
+            
+            
+           
+            j++;
+        }
+          System.out.println(handValue);
+          
+          if(handValue==21){
+          System.out.println("blackjack"); 
+          System.exit(0);
+        }
+          
+        if ( hos==0){
+            System.out.println("standing");
+            if(aceCount>=1){
+                
+            }
+        }
+      
+        if(handValue>21){
+          System.out.println("bust");  
+          System.exit(0);
+        }
+      
+        return handValue;
+    }
+    
+    
+    public static int calculateHandd(int hos){
+        int j=0;
+        int handValue=0;
+        int aceCount=0;
+        while (Hand.pHand[j] !=null){
+            String o;
+            o = Hand.pHand[j].getValue().toString();
+            
+           //System.out.println(o);
+           
+            switch(o){
+                case "ACE":
+                         handValue+=11;
+                         System.out.println("11");
+                       
+                    break;
+                    case "TWO":
+                    System.out.println("2");
+                    handValue+=2;
+                    break;
+                    case "THREE":
+                    System.out.println("3");
+                    handValue+=3;
+                    break;
+                    case "FOUR":
+                    System.out.println("4");
+                    handValue+=4;
+                    break;
+                    case "FIVE":
+                    System.out.println("5");
+                    handValue+=5;
+                    break;
+                    case "SIX":
+                    System.out.println("6");
+                    handValue+=6;
+                    break;
+                    case "SEVEN":
+                    System.out.println("7");
+                    handValue+=7;
+                    break;
+                    case "EIGHT":
+                    System.out.println("8");
+                    handValue+=8;
+                    break;
+                    case "NINE":
+                    System.out.println("9");
+                    handValue+=9;
+                    break;
+                    case "TEN":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "JACK":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "QUEEN":
+                        System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    case "KING":
+                    System.out.println("10");
+                    handValue+=10;
+                    
+                    break;
+                    
+            }
+            
+            
+           
+            
+            
+            
+           
+            j++;
+        }
+          System.out.println(handValue);
+          
+          if(handValue==21){
+          System.out.println("blackjack"); 
+          System.exit(0);
+          
+        }
+          
+          else if ( hos==0){
+            System.out.println("standing");
+            if(handValue>=22){
+          System.out.println("bust");  
+          Runtime.getRuntime().halt(0);
+        }
+        }
+      
+          
+      
+        return handValue;
+    }
+    
+    
 
 }
